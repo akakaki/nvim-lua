7b743426ddeebce52318e392550a5a7b16432b2e
Amuuuuuuuu <oupquv@gmail.com>
2023-11-27 12:01:09 +0800

    add nvim tree toggle side

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -1,8 +1,15 @@
 -- https://github.com/nvim-tree/nvim-tree.lua/blob/master/doc/nvim-tree-lua.txt
 
-vim.api.nvim_set_keymap("n", "<C-E>", "::NvimTreeToggle<CR>", {})
+vim.api.nvim_set_keymap("n", "<C-E>", ":NvimTreeToggle<CR>", {})
+
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  vim.keymap.set("n", "<C-E>", api.tree.toggle)
+end
 
 require("nvim-tree").setup({
+  on_attach = on_attach,
   renderer = {
     icons = {
       show = {
