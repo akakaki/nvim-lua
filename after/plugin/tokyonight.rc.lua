404bcaffa79cffbdd74ad9def6233d262a8aa839
Amuuuuuuuu <oupquv@gmail.com>
2023-11-28 18:20:44 +0800

    [Feature] update basic

--- after/plugin/tokyonight.rc.lua
+++ after/plugin/tokyonight.rc.lua
@@ -1,2 +1,2 @@
-vim.cmd[[colorscheme tokyonight-day]]
+vim.cmd[[colorscheme tokyonight]]
 
--- lua/basic.lua
+++ lua/basic.lua
@@ -9,11 +9,20 @@ vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 
 vim.opt.number = true
--- vim.opt.autoindent = true
 vim.opt.backup = false
 vim.opt.shiftwidth = 2
 vim.opt.cursorline = true
 
 vim.opt.wildoptions = "pum"
 vim.opt.pumblend = 4
+
+-- 只有寫前端的時候才需用上，為了符合 vscode 共同作業的夥伴們
+vim.api.nvim_create_autocmd("FileType",{
+  pattern = { "javascript", "html", "vue" },
+  callback = function()
+    vim.opt.tabstop = 2
+    vim.opt.autoindent = true
+    vim.opt.expandtab = true
+  end
+})
 
